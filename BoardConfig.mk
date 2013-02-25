94e0eb044a8dae64f804def76db94887d34b8e2b
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-02-25 15:21:21 +0100

    yuga: board update
    * fix kernel branch
    * rename kernel config
    
    Change-Id: I724150294dd706e63eb658156cfaef48f7791289

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ TARGET_SPECIFIC_HEADER_PATH += device/sony/yuga/include
 
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/yuga/bluetooth
 
-TARGET_KERNEL_CONFIG := cyanogen_fusion3_yuga_defconfig
+TARGET_KERNEL_CONFIG := cm_fusion3_yuga_defconfig
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 26
