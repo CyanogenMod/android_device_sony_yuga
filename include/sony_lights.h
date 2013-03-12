e26c9cf26e64712df74a93087f9b3a9dab138841
Alin Jerpelea <jerpelea@gmail.com>
2013-03-12 18:56:21 +0200

    yuga:liblights: typo fix
    Change-Id: Ie75a360b35e5f386bda61b0b7eab8b1bc806bdf2

--- include/sony_lights.h
+++ include/sony_lights.h
@@ -15,8 +15,8 @@
  * limitations under the License.
  */
 
-char const*const LCD_BACKLIGHT_FILE		= "/sys/class/leds/lm3533-lcd-bl-1/brightness";
-char const*const LCD_BACKLIGHT2_FILE		= "/sys/class/leds/lm3533-lcd-bl-2/brightness";
+char const*const LCD_BACKLIGHT_FILE		= "/sys/class/leds/lm3533-lcd-bl/brightness";
+char const*const LCD_BACKLIGHT2_FILE		= "";
 char const*const RED_LED_FILE			= "/sys/class/leds/lm3533-red/brightness";
 char const*const GREEN_LED_FILE			= "/sys/class/leds/lm3533-green/brightness";
 char const*const BLUE_LED_FILE			= "/sys/class/leds/lm3533-blue/brightness";
