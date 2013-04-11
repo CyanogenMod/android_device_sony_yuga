72649a9169d63fd3437a594015e60fc42662a44a
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-04-11 22:40:17 +0200

    yuga: add camera blobs
    Change-Id: I043b2320190daee6bd03b6ee7e3e876a4c53fe1c

--- yuga.mk
+++ yuga.mk
@@ -30,7 +30,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Configuration scripts
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/config/flashled_calc_parameters.cfg:system/etc/flashled_calc_parameters.cfg \
     $(LOCAL_PATH)/config/sensors.conf:system/etc/sensors.conf \
     $(LOCAL_PATH)/prebuilt/hw_config.sh:system/etc/hw_config.sh \
     $(LOCAL_PATH)/prebuilt/pre_hw_config.sh:system/etc/pre_hw_config.sh
