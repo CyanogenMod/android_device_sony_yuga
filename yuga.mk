b2de3a65e77f5710a0d7dd28039d42472a18625a
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-05-17 20:13:22 +0200

    yuga: add proximity permission
    Change-Id: I48fccb5137526211e3a9698e6b90720bc8b33780

--- yuga.mk
+++ yuga.mk
@@ -20,6 +20,7 @@ DEVICE_PACKAGE_OVERLAYS += device/sony/yuga/overlay
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
 # This device is xhdpi.  However the platform doesn't
