b22aa70d0ae1c26aeb2dbef8120be6120401da97
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-05-19 21:56:52 +0200

    yuga: update signature
    Change-Id: Id3c082c5a77163837d7e03e2c19f6947e6779994

--- cm.mk
+++ cm.mk
@@ -13,7 +13,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603_1270-4975 BUILD_FINGERPRINT=Sony/C6603_1270-4975/C6603:4.1.2/10.1.A.1.434/Tfp_rw:user/release-keys PRIVATE_BUILD_DESC="C6603-user 4.1.2 10.1.A.1.434 Tfp_rw test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603_1270-7298/C6603:4.1.2/10.1.1.A.1.253/Avt3rw:user/release-keys PRIVATE_BUILD_DESC="6603-user 4.1.2 10.1.1.A.1.253 Avt3rw test-keys"
 
 PRODUCT_NAME := cm_yuga
 PRODUCT_DEVICE := yuga
