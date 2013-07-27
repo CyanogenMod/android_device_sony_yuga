62cfcf5e2c907a4738267cb31cc7914817ab04f1
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-07-27 04:39:28 +0200

    yuga: switch to mr2 audio
    Change-Id: Ib9453a6e2bbac15f4a62b4ad1bf06e650089c4e2

--- yuga.mk
+++ yuga.mk
@@ -54,8 +54,7 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/rootdir/system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3:system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3
+    $(LOCAL_PATH)/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf
 
 # Touchpad
 PRODUCT_COPY_FILES += \
