260d22b098559ccd90a46e346006573c1be9c691
sssemil <suleymanovemil8@gmail.com>
2014-09-20 15:06:00 +0500

    yuga: update fingerprint from 10.5.1.A.0.283
    Change-Id: I43ef7f614b78bfa07646fbf086758cc2b4865907

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603_1270-6697/C6603:4.4.2/10.5.A.0.230/WP5_rg:user/release-keys PRIVATE_BUILD_DESC="yuga-userdebug 4.4.2 10.5.A.0.230 WP5_rg test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603/C6603:4.4.4/10.5.1.A.0.283/5P53rQ:user/release-keys PRIVATE_BUILD_DESC="C6603-user 4.4.4 10.5.1.A.0.283 5P53rQ test-keys"
 
 PRODUCT_NAME := cm_yuga
 PRODUCT_DEVICE := yuga
