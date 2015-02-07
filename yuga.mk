a85df73c51ebaa1204988b9e50f42567b7a96c30
Fu-Chun Hsu <hsufuchun@gmail.com>
2015-02-08 07:35:03 +0800

    [WIP] audio: Changes for new audio HAL
    * Deprecate old defines
    * Add custom audio_platform_info.xml
    
    Change-Id: Ieac449a32936dee45ebbce503397a48253b99667

--- yuga.mk
+++ yuga.mk
@@ -58,6 +58,7 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/system/etc/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml
 
