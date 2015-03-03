fdf01eaa59e63ef587469b089d88cee0b542b5e6
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-03-03 13:02:26 +0100

    use half-res boot animation
    Change-Id: I5feae96b95449704e9513f993a2c656b2ce12403

--- full_yuga.mk
+++ full_yuga.mk
@@ -22,8 +22,10 @@
 #
 #
 
+# Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOTANIMATION_HALF_RES := true
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
