b7a74ef5e73b92a6b674852646db5120ba67d96b
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-03-04 10:01:54 +1030

    yuga:  remove deprecated vold.fstab
    Obsolete since 4.3
    
    Change-Id: Id8d754841f941b17354d6b43182b6170a2072c31

--- yuga.mk
+++ yuga.mk
@@ -52,10 +52,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.sony.usb.rc:root/init.sony.usb.rc
 
-# Vold
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/system/etc/vold.fstab:system/etc/vold.fstab
-
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
