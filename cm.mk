6db6eaf3758cf83d79bc2b8faa01887b9f252abc
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-09-10 12:42:00 +0200

    update build fingerprint
    Change-Id: Id5191f75eaed9233f4501502989b94c32da46be0

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603/C6603:5.0.2/10.6.A.0.454/266452109:user/release-keys PRIVATE_BUILD_DESC="C6603-user 5.0.2 10.6.A.0.454 266452109 test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603/C6603:5.1.1/10.7.A.0.222/1314554057:user/release-keys PRIVATE_BUILD_DESC="C6603-user 5.1.1 10.7.A.0.222 1314554057 test-keys"
 
 PRODUCT_NAME := cm_yuga
 PRODUCT_DEVICE := yuga
