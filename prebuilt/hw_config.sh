e536cdc4619133d90912592564a493b6ccbaf1fe
Tetsuo Osaka <tetsugit@gmail.com>
2016-11-11 02:34:23 +0100

    yuga: remove unused config.sh prebuilts
    These appear to be completely useless as the binary they depend on
    was removed in firmware update 222
    
    Change-Id: Ia218c8b6c345fd63d298b7c038cbec54cd01fb33

--- prebuilt/hw_config.sh
+++ prebuilt/hw_config.sh
@@ -10,17 +10,6 @@ val_freq=2
 val_threshold=4
 val_filter=1
 
-ta_param_loader 60240 prox_cal
-val_calibrated=$?
-case $val_calibrated in
- 1)
-  ta_param_loader 60240 threshold
-  val_threshold=$?
-  ta_param_loader 60240 rfilter
-  val_filter=$?
-  ;;
-esac
-
 echo $val_cycle > $dev/cycle    # Duration Cycle. Valid range is 0 - 3.
 echo $val_nburst > $dev/nburst  # Number of pulses in burst. Valid range is 0 - 15.
 echo $val_freq > $dev/freq      # Burst frequency. Valid range is 0 - 3.
